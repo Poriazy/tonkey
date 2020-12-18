f913c746d790e1de2756179d694e8024c4908b18
MohamadAli <mrht74@yahoo.com>
2020-12-18 05:40:24 +0330

    Adopted PEP8

--- src/models/groups.py
+++ src/models/groups.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-from sqlalchemy import create_engine, MetaData, Table, Column, BigInteger, \
-Float, Integer, SmallInteger, String, Text, DateTime, FetchedValue, text, func
+from sqlalchemy import create_engine, MetaData, Table, Column, func, \
+    Float, SmallInteger, String, Text, DateTime, FetchedValue, text
 
 metadata = MetaData()
 
@@ -10,42 +10,41 @@ engine = create_engine(
     connection_string, echo=True, pool_size=6, max_overflow=10)
 
 groups = Table("groups", metadata,
-            Column("id",
-                   SmallInteger(),
-                   primary_key=True),
-            Column("code",
-                   SmallInteger(),
-                   index=True,
-                   unique=True,
-                   nullable=False),
-            Column("title",
-                   # TODO check this
-                   #String(100, collation="fa_IR.utf8"),
-                   #Text(collation="fa_IR.utf8"),
-                   #Text(),
-                   String(100),
-                   index=True,
-                   nullable=False),
-            Column("description",
-                   Text(),
-                   nullable=False,
-                   # TODO: correct other tables like below
-                   server_default="",
-                   server_onupdate=""),
-            Column("pe",
-                   Float(),
-                   nullable=False,
-                   # TODO: correct other tables like below
-                   server_default=text("0")),
-            Column("time_stamp",
-                   DateTime(),
-                   nullable=False,
-                   # TODO: check this
-                   #server_onupdate=FetchedValue()))
-                   #default=datetime.now,
-                   # TODO: correct other tables like below
-                   server_default=func.now(),
-                   server_onupdate=func.now()))
+               Column("id",
+                      SmallInteger(),
+                      primary_key=True),
+               Column("code",
+                      SmallInteger(),
+                      index=True,
+                      unique=True,
+                      nullable=False),
+               Column("title",
+                      # TODO check this
+                      #String(100, collation="fa_IR.utf8"),
+                      # Text(collation="fa_IR.utf8"),
+                      # Text(),
+                      String(100),
+                      index=True,
+                      nullable=False),
+               Column("description",
+                      Text(),
+                      nullable=False,
+                      # TODO: correct other tables like below
+                      server_default="",
+                      server_onupdate=""),
+               Column("pe",
+                      Float(),
+                      nullable=False,
+                      # TODO: correct other tables like below
+                      server_default=text("0")),
+               Column("time_stamp",
+                      DateTime(),
+                      nullable=False,
+                      # TODO: check this
+                      # server_onupdate=FetchedValue()))
+                      # default=datetime.now,
+                      # TODO: correct other tables like below
+                      server_default=func.now(),
+                      server_onupdate=func.now()))
 
 metadata.create_all(engine)
-
--- src/models/markets.py
+++ src/models/markets.py
@@ -1,41 +1,39 @@
 from datetime import datetime
 
-from sqlalchemy import create_engine, MetaData, Table, Column, BigInteger, \
-        Float, Integer, SmallInteger, String, Text, DateTime
+from sqlalchemy import create_engine, MetaData, Table, Column, SmallInteger, \
+    Text, DateTime, String
 
 metadata = MetaData()
 
 connection_string = 'postgresql://debman:password@localhost/bsg'
 engine = create_engine(
-        connection_string, echo=True, pool_size=6, max_overflow=10)
+    connection_string, echo=True, pool_size=6, max_overflow=10)
 
 markets = Table("markets", metadata,
-            Column("id",
-                   SmallInteger(),
-                   primary_key=True),
-            Column("code",
-                   SmallInteger(),
-                   index=True,
-                   unique=True,
-                   nullable=False),
-            Column("title",
-                   # TODO check this
-                   #Unicode(100),
-                   #String(100, collation='utf-8'),
-                   #String(100),
-                   Text(),
-                   index=True,
-                   nullable=False),
-            Column("description",
-                   Text(),
-                   nullable=False,
-                   default="",
-                   onupdate=""),
-            Column("time_stamp",
-                   DateTime(),
-                   nullable=False,
-                   default=datetime.now,
-                   onupdate=datetime.now))
+                Column("id",
+                       SmallInteger(),
+                       primary_key=True),
+                Column("code",
+                       SmallInteger(),
+                       index=True,
+                       unique=True,
+                       nullable=False),
+                Column("title",
+                       # TODO check this
+                       #String(100, collation='utf-8'),
+                       # String(100),
+                       Text(),
+                       index=True,
+                       nullable=False),
+                Column("description",
+                       Text(),
+                       nullable=False,
+                       default="",
+                       onupdate=""),
+                Column("time_stamp",
+                       DateTime(),
+                       nullable=False,
+                       default=datetime.now,
+                       onupdate=datetime.now))
 
 metadata.create_all(engine)
-
